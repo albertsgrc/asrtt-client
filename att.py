f9ca5eeeb422418d7e1db8dcfbfe787a79a1c7d5
Albert Segarra Roca <albertsgrc@gmail.com>
2018-12-02 17:41:10 +0100

    Finish

--- att.py
+++ att.py
@@ -1,19 +1,50 @@
 #!/usr/bin/env /usr/local/bin/python3
 
-from pynput import mouse, keyboard
-from time import time
-from urllib.request import Request, urlopen
+import time
+import json
 from threading import Timer
+from os import getenv, getcwd
+import re
+
+from git import Repo
+from pynput import mouse, keyboard
+import click
 from logzero import logger
 import logzero
-import logging
-from os import getenv
+import inquirer
+import validators
+import requests
 
-SHOULD_TRACK_URL = getenv('ATT_SHOULD_TRACK_URL')
-SET_IS_WORKING_URL = getenv('ATT_SET_IS_WORKING_URL')
-SET_NOT_WORKING_URL = getenv('ATT_SET_NOT_WORKING_URL')
 
-LOG_FILE = getenv('ATT_LOG_FILE')
+from configstore import ConfigStore
+
+
+def get_current_branch_name(repo_path):
+    return Repo(repo_path).active_branch.name
+
+
+def get_remote_info(repo_path):
+    url = Repo(repo_path).remotes.origin.url
+    match = re.match(r"^(git@|https:\/\/)([\w.]+)(?:\/|:)(.*\/*.)\.git$", url)
+
+    return {
+        "hostname": match.group(2),
+        "project": match.group(3)
+    }
+
+
+def client_data():
+    data_dict = conf.all()
+
+    repository_path = conf.get('repositoryPath')
+
+    remote_info = get_remote_info(repository_path)
+
+    data_dict = {**remote_info, **data_dict}
+
+    data_dict['gitBranch'] = get_current_branch_name(repository_path)
+
+    return data_dict
 
 
 def rate_limit(rate):
@@ -23,7 +54,7 @@ def rate_limit(rate):
 
         def wrapper(*args, **kwargs):
             global last_execution
-            current_time = time()
+            current_time = time.time()
             if current_time - last_execution > rate:
                 last_execution = current_time
                 fn(*args, **kwargs)
@@ -33,25 +64,22 @@ def rate_limit(rate):
     return decorator_limit
 
 
-def send_post(url):
-    request = Request(url, b'')
+def send_post(url, json={}):
     try:
-        urlopen(request)
+        requests.post(url, json=json)
     except:
-        logger.warning(f'cannot reach url {url}')
+        logger.error(f'cannot POST {url}')
 
 
 def send_get(url):
-    request = Request(url)
-
-    content = b'n'
+    result = {}
 
     try:
-        content = urlopen(request).read()
+        requests.get(url).json()
     except:
-        logger.warning(f'cannot reach url {url}')
+        logger.error(f'cannot GET {url}')
 
-    return content
+    return result
 
 
 class AliveNotifier:
@@ -64,7 +92,7 @@ class AliveNotifier:
     def notify(self):
         if self.alive:
             logger.info('alive notification')
-            send_post(self.url)
+            send_post(self.url, client_data())
             self.timer = Timer(self.interval, self.notify)
             self.timer.start()
 
@@ -86,7 +114,8 @@ class Tracker:
         self.stopped_working_timer = None
         self.started = False
         self.max_idle_time = max_idle_time
-        self.alive_notifier = AliveNotifier(set_is_working_url, max_idle_time/2)
+        self.alive_notifier = AliveNotifier(
+            set_is_working_url, max_idle_time/2)
         self.set_not_working_url = set_not_working_url
 
     def start(self):
@@ -128,7 +157,7 @@ class Tracker:
         self.is_working = False
         self.alive_notifier.stop()
         logger.info('stop working')
-        send_post(self.set_not_working_url)
+        send_post(self.set_not_working_url, client_data())
 
     def set_max_idle_time(self, value):
         if self.max_idle_time != value:
@@ -162,20 +191,21 @@ class TrackerManager:
         self.check_should_track()
 
     def check_should_track(self):
-        response = send_get(self.should_track_url).decode('utf-8')
+        response = send_get(self.should_track_url)
 
         self.timer = Timer(10, self.check_should_track)
         self.timer.start()
 
-        if response == 'n':
+        if 'maxIdleTime' in response:
+            max_idle_time = response['maxIdleTime']
+            logger.info(f'should track with max_idle_time={max_idle_time}')
+            self.tracker.set_max_idle_time(max_idle_time)
+            if not self.tracker.is_running():
+                self.tracker.start()
+        else:
             logger.info('should not track')
             if self.tracker.is_running():
                 self.tracker.stop()
-        else:
-            logger.info(f'should track with max_idle_time={int(response)}')
-            self.tracker.set_max_idle_time(int(response))
-            if not self.tracker.is_running():
-                self.tracker.start()
 
     def stop(self):
         if self.timer:
@@ -185,21 +215,140 @@ class TrackerManager:
             self.tracker.stop()
 
 
+def is_git_repo(path):
+    is_repo = True
+    try:
+        Repo(getcwd())
+    except:
+        is_repo = False
 
-def main():
-    if SHOULD_TRACK_URL == None or SET_IS_WORKING_URL == None or SET_NOT_WORKING_URL == None:
-        logger.error(f'One or more server URL(s) are missing')
-    
-    if LOG_FILE != None:
-        try:
-            logzero.logfile(LOG_FILE)
-        except:
-            logger.error(f'Invalid log file {LOG_FILE}')
+    return is_repo
 
-    tracker_manager = TrackerManager(SHOULD_TRACK_URL, SET_IS_WORKING_URL, SET_NOT_WORKING_URL)
+
+def initialize_config(conf):
+    def gitlabTokenValidator(others, token):
+        if token == '':
+            return True
+
+        repository_path = others['repositoryPath']
+
+        info = get_remote_info(repository_path)
+        hostname = info['hostname']
+        result = requests.get(f'https://{hostname}/api/v4/version',
+                              headers={"private-token": token})
+
+        if result.status_code == 404:
+            print('')
+            logger.error(
+                f'Repository {repository_path} with remote {hostname} is probably not a gitlab remote')
+
+        return result.status_code == 200
+
+    def togglTokenValidator(_, token):
+        result = requests.get('https://www.toggl.com/api/v8/me',
+                              auth=(token, 'api_token'))
+
+        return result.status_code == 200
+
+    def gitRepoValidator(_, path):
+        return is_git_repo(path)
+
+    def serverUrlValidator(_, url):
+        return validators.url(url)
+
+    def transformServerUrl(url):
+        if not url.endswith('/'):
+            return url + '/'
+        else:
+            return url
+
+    repoDefault = (getcwd() + '/') if is_git_repo(getcwd()) else None
+
+    questions = [
+        inquirer.Text('repositoryPath', message="Enter your repository path",
+                      default=repoDefault, validate=gitRepoValidator),
+        inquirer.Text('gitlabToken', message="Enter your gitlab token", default='',
+                      validate=gitlabTokenValidator),
+        inquirer.Text('togglToken', message="Enter your toggl token",
+                      validate=togglTokenValidator),
+        inquirer.Text(
+            'serverUrl', message="Enter the tracking server url", validate=serverUrlValidator),
+        inquirer.Path('logsFile', message="Enter the logs file path",
+                      path_type=inquirer.Path.FILE, default="/tmp/att.log")
+    ]
+
+    answers = inquirer.prompt(questions)
+
+    answers['serverUrl'] = transformServerUrl(answers['serverUrl'])
+
+    conf.all(answers)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command('start', short_help='Start tracking time')
+def start():
+    global tracker_manager
+
+    server_url = conf.get('serverUrl')
+
+    should_track_url = server_url + 'should-track'
+    set_is_working_url = server_url + 'set-is-working'
+    set_not_working_url = server_url + 'set-not-working'
+
+    tracker_manager = TrackerManager(
+        should_track_url, set_is_working_url, set_not_working_url)
 
     tracker_manager.start()
 
+
+@click.command('reset-config', short_help='Reset configuration')
+def reset_config():
+    if not justInitialized:
+        initialize_config(conf)
+
+
+@click.command('get-repo', short_help='Print the current git repository directory')
+def get_repo():
+    current = conf.get('repositoryPath')
+    logger.info(f'Current repository path is {current}')
+
+
+@click.command('set-repo', short_help='Set the current git repository directory')
+@click.option('--path', '-p', default=getcwd(), help="Set the repository directory")
+def set_repo(path):
+    conf.set('repositoryPath', path)
+    logger.info(f'Repository path set to {path}')
+
+
+@click.command('get-config', short_help='Print the current configuration')
+def get_config():
+    print(json.dumps(client_data(), sort_keys=True, indent=4))
+
+
+cli.add_command(start)
+cli.add_command(reset_config)
+cli.add_command(set_repo)
+cli.add_command(get_repo)
+cli.add_command(get_config)
+
+
+def main():
+    global conf
+    conf = ConfigStore('att')
+
+    global justInitialized
+    justInitialized = conf.size == 0
+
+    if conf.size == 0:
+        initialize_config(conf)
+
+    logzero.logfile(conf.get('logsFile'))
+
+    cli()
 
 
 if __name__ == "__main__":
@@ -211,6 +360,3 @@ if __name__ == "__main__":
 # branch_name = head.name
 
 # Gitlab: https://python-gitlab.readthedocs.io/en/stable/gl_objects/issues.html
-
-
-
--- setup.py
+++ setup.py
@@ -3,22 +3,20 @@ from setuptools import setup
 setup(
     name='att',
     version='0.10',
-    py_modules=['att', 'clockify'],
+    py_modules=['att.py', 'configstore.py', 'utils.py'],
     author='Albert Segarra',
     url='https://github.com/albertsgrc/att-client',
     install_requires=[
-        'pyinput',
-        'logzero',
+        'pynput==1.4',
+        'logzero==1.5.0',
         'click==6.7',
-        'certifi==2018.8.13',
-        'chardet==3.0.4',
-        'click==6.7',
-        'idna==2.7',
         'requests>=2.20.0',
-        'urllib3==1.23',
+        'gitpython==2.1.11',
+        'inquirer==2.5.1',
+        'validators==0.12.3',
     ],
     entry_points='''
         [console_scripts]
         att=att.att:main
-   ''', 
+   ''',
 )
