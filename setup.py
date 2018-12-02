47306525c0be712198ed2b55697384f49e5c2533
Albert Segarra Roca <albertsgrc@gmail.com>
2018-12-02 21:25:14 +0100

    Change version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="asrtt",
-    version="0.0.3",
+    version="1.0.0",
     author="Albert",
     description="An automated time tracker",
     long_description=long_description,
